5886c09cb136d0f592639e965b11abf1be31a7c0
南通伍林堂文化传播有限公司 <15062733214@qq.com>
2022-09-04 10:15:50 +0800

    Update copyright.tsx

--- src/components/global/copyright.tsx
+++ src/components/global/copyright.tsx
@@ -36,14 +36,13 @@ return (
   <p>
     <img src="/mabai.png"/>
     <img src="/hanzuwang.png"/></p>
+<p>CDN数据源存储于<a href="https://cloud.tencent.com/act/cps/redirect?redirect=2182&cps_key=49716edce3a6c8b2f1df7249791dcd72&from=console" target="_blank" rel="nofollow">腾讯云</a>，中继IP由 <a href="https://www.ccwadj.com/" target="_blank" rel="nofollow">云之道数据</a> 提供区域分配</p>
   <p>
     <a target="_blank" rel="nofollow" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32061202001219">苏公网安备32061202001219号</a>、
     <a href="https://beian.miit.gov.cn/" target="_blank">苏ICP备2021008246号-19</a></p>
   <p>©{copyright.startTime}-{endTime}
     <a href={copyright.href} target="_blank">{copyright.author}</a>版权所有</p>
-  <p>本站由
-    <a target="_blank" rel="nofollow" href="http://www.ccwadj.cn/">云之道数据</a>提供云计算服务</p>
-  <p>
+
     <a target="_blank" rel="nofollow" href="https://www.wulintang.xin/new/202107081.shtml">
       <img src="https://www.abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcba.cn/yun/yun.png" /></a>
   </p>
