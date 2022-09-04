7db20b15109b513918f53c094fb55fbf44142f9b
南通伍林堂文化传播有限公司 <15062733214@qq.com>
2022-09-04 10:23:50 +0800

    Update copyright.tsx

--- src/components/global/copyright.tsx
+++ src/components/global/copyright.tsx
@@ -36,7 +36,10 @@ return (
   <p>
     <img src="/mabai.png"/>
     <img src="/hanzuwang.png"/></p>
-<p>CDN数据源存储于<a href="https://cloud.tencent.com/act/cps/redirect?redirect=2182&cps_key=49716edce3a6c8b2f1df7249791dcd72&from=console" target="_blank" rel="nofollow">腾讯云</a>，中继IP由 <a href="https://www.ccwadj.com/" target="_blank" rel="nofollow">云之道数据</a> 提供区域分配</p>
+<p>
+  CDN数据源存储于<a href="https://cloud.tencent.com/act/cps/redirect?redirect=2182&cps_key=49716edce3a6c8b2f1df7249791dcd72&from=console" target="_blank">腾讯云</a>，
+  中继IP由 <a href="https://www.ccwadj.com/" target="_blank">云之道数据</a> 提供区域分配
+</p>
   <p>
     <a target="_blank" rel="nofollow" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32061202001219">苏公网安备32061202001219号</a>、
     <a href="https://beian.miit.gov.cn/" target="_blank">苏ICP备2021008246号-19</a></p>
