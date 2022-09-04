d7de0b9fa187b8b847ad946491c4581ee061e6f0
南通伍林堂文化传播有限公司 <15062733214@qq.com>
2022-09-04 22:38:13 +0800

    Update copyright.tsx

--- src/components/global/copyright.tsx
+++ src/components/global/copyright.tsx
@@ -36,9 +36,8 @@ return (
         <p><img src="/mabai.png" /><img src="/hanzuwang.png" /></p>
         <p>CDN数据源存储于 <a target="_blank" href="https://cloud.tencent.com/act/cps/redirect?redirect=2182&cps_key=49716edce3a6c8b2f1df7249791dcd72&from=console">腾讯云</a>、中继IP由 <a target="_blank" href="https://www.ccwadj.com/">云之道数据</a> 提供区域分配</p>
         <p><a target="_blank" rel="nofollow" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32061202001219">苏公网安备32061202001219号</a>、<a href="https://beian.miit.gov.cn/" target="_blank">苏ICP备2021008246号-19</a></p>
-        <p>©{copyright.startTime}-{endTime}<a href={copyright.href} target="_blank">{copyright.author}</a>版权所有</p>
+        <p>©{copyright.startTime}-{endTime}<a href={copyright.href} target="_blank">{copyright.author}</a>版权所有、<a target="_blank" title="51la网站统计" href="https://v6.51.la/land/JfZgAjaA6gGu2lST">统计</a></p>
         <p><a target="_blank" rel="nofollow" href="https://www.wulintang.xin/new/202107081.shtml"><img src="https://www.abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcba.cn/yun/yun.png" /></a></p>
-        <p><a target="_blank" title="51la网站统计" href="https://v6.51.la/land/JfZgAjaA6gGu2lST"><img src="https://sdk.51.la/icon/1-4.png" /></a></p>
 </div>
   );
 };
